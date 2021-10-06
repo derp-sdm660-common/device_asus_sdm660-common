b5ef2eed61a1ba5948787d3c9b053f9d689cfef4
Immanuel44 <Immanuel44@users.noreply.github.com>
2021-10-06 18:39:01 +0530

    sdm660-common: Checkout Sensor Blobs from LA.UM.9.6.2.r1-04100-89xx.0
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -411,8 +411,10 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.frameworks.sensorservice@1.0 \
+    android.hardware.sensors@1.0-impl:64 \
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
